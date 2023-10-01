b0a7f3bcc9cb7940c02459ee1d5384a3ca0a5dbc
Richard H <tevesek3@gmail.com>
2023-10-01 15:12:28 +0200

    Create GUI instead of console app

--- main.py
+++ main.py
@@ -3,13 +3,11 @@ import platform
 import service.read_manager as read_manager
 import service.xml_manager as xml_manager
 import service.folder_selector_manager as folder_selector_manager
-from tkinter import Tk
+from tkinter import Tk, Button, Label, Listbox, Scrollbar, messagebox
 
 INPUT_THEME_JSON = "themes.json"
-SEPARATOR_LINE = "----------------------------------------------"
 
 def show_picture(picture_path):
-    
     if picture_path is not None:
         system = platform.system()
         if system == "Darwin":  # macOS
@@ -23,86 +21,81 @@ def show_picture(picture_path):
     else:
         print("No picture found for the selected theme.")
 
-def get_input_folder_location():
-    input_folder_location = folder_selector_manager.find_db_commander_property_file()
-
-    while True:
-        if os.path.isfile(input_folder_location):
-            print("✔️  Folder location is valid.")
-            return input_folder_location
-        else:
-            print("Invalid input folder location.")
-
-def create_root_tinkter():
-    root = Tk()
-    root.withdraw()  # Hide the root window
+def apply_theme(theme_option, db_folder_location, listbox):
+    xml_manager.apply_changes(db_folder_location, theme_option["file_path"])
+    messagebox.showinfo("Theme Applied", f"{theme_option['name']} applied successfully.")
 
 def main():
+    def create_root_tkinter():
+        root = Tk()
+        root.withdraw()  # Hide the root window
+
+    create_root_tkinter()
     last_applied_theme = ""
-    create_root_tinkter()
     theme_list = read_manager.read_themes_as_list(INPUT_THEME_JSON)
 
-    print(SEPARATOR_LINE)
-    print("⚠️  If you've never opened Double Commander before, please do so, and close it.\n(It creates doublecmd.xml which is essential!)")
-    print(SEPARATOR_LINE)
-    print("Please select Double Commander installation location.")
-    print("0. Exit")
-    print("1. OK")
+    root = Tk()
+    root.title("Double Commander Theme Selector")
 
-    if (input() == "0"):
-        print("Exiting...")
-        return
+    def get_input_folder_location():
+        input_folder_location = folder_selector_manager.find_db_commander_property_file()
+        while True:
+            if os.path.isfile(input_folder_location):
+                return input_folder_location
+            else:
+                messagebox.showerror("Invalid Input Folder", "Invalid input folder location. Please select again.")
+
+    def on_quit():
+        root.quit()
+
+    def on_theme_select():
+        selected_index = themes_listbox.curselection()
+        if not selected_index:
+            messagebox.showwarning("No Theme Selected", "Please select a theme from the list.")
+            return
+
+        selected_theme = theme_list[selected_index[0]]
+        show_picture(selected_theme["picture_path"])
+
+    def on_theme_apply():
+        selected_index = themes_listbox.curselection()
+        if not selected_index:
+            messagebox.showwarning("No Theme Selected", "Please select a theme from the list.")
+            return
+
+        selected_theme = theme_list[selected_index[0]]
+        apply_theme(selected_theme, db_folder_location, themes_listbox)
+        nonlocal last_applied_theme
+        last_applied_theme = selected_theme["name"]
+
+    def refresh_theme_list():
+        themes_listbox.delete(0, "end")
+        for theme in theme_list:
+            themes_listbox.insert("end", theme["name"])
 
     db_folder_location = get_input_folder_location()
 
-    while True:
-        print(SEPARATOR_LINE)
-        print("Theme Options:")
-        print("0. Exit")
+    # Theme List
+    themes_listbox = Listbox(root, selectmode="SINGLE", height=10)
+    themes_listbox.grid(row=0, column=0, padx=10, pady=10)
+    refresh_theme_list()
 
-        for i, theme in enumerate(theme_list, start=1):
-            list_item_name = f"{i}. {theme['name']}"
-            
-            if theme["name"] == last_applied_theme:
-                list_item_name += "⭐*"
-            print(list_item_name)
+    scrollbar = Scrollbar(root, orient="vertical")
+    scrollbar.grid(row=0, column=1, sticky="ns")
+    themes_listbox.config(yscrollcommand=scrollbar.set)
+    scrollbar.config(command=themes_listbox.yview)
 
-        theme_size = len(theme_list)
-        print(SEPARATOR_LINE)
-        print(f"Select a theme option (1-{theme_size}): ")
-        theme_input = int(input())
+    # Buttons
+    show_button = Button(root, text="Show Picture", command=on_theme_select)
+    show_button.grid(row=1, column=0, padx=10, pady=5, sticky="w")
 
-        if theme_input == 0:
-            print("Exiting...")
-            break
+    apply_button = Button(root, text="Apply Theme", command=on_theme_apply)
+    apply_button.grid(row=1, column=0, padx=10, pady=5, sticky="e")
 
-        for i in range(theme_size):
-            if theme_input - 1 == i:
-                theme_option = theme_list[i]
-                print("✔️ *", theme_list[i]["name"], "selected.")
-                break
-        
-        while True:
-            print(SEPARATOR_LINE)
-            print("0. Back to main menu")
-            print("1. Apply theme")
-            print("2. Show picture")
-            apply_input = int(input())
+    quit_button = Button(root, text="Quit", command=on_quit)
+    quit_button.grid(row=2, column=0, padx=10, pady=10, sticky="e")
 
-            if (apply_input == 0):
-                print("Back to main menu.")
-                break
+    root.mainloop()
 
-            if (apply_input == 1):
-                xml_manager.apply_changes(db_folder_location, theme_option["file_path"])
-                print(SEPARATOR_LINE)
-                print(f"*** {theme_option['name']} applied. ***")
-                last_applied_theme = theme_option["name"]
-                break
-
-            if (apply_input == 2):
-                print("Showing picture...")
-                show_picture(theme_option["picture_path"])
-                continue
-
-main()
+if __name__ == "__main__":
+    main()
